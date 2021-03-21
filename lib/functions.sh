ed091054879914d9dc06db1c39d381592a248239
nvanlaerebeke <crazytje+github@gmail.com>
2021-03-21 21:02:55 +0100

    added some more logging

--- lib/functions.sh
+++ lib/functions.sh
@@ -40,18 +40,33 @@ function upgrade {
         then
             echo "Failed getting deployments"
             exit 1
+        else
+            echo "Found deployment"
+            echo $DEPLOYMENT | jq
         fi
+
         local DEPLOYED_VERSION=`echo $DEPLOYMENT | jq -r '.app_version'`
         local TARGETNAME=`echo $DEPLOYMENT | jq -r '.name'`
         
         if [ $DEPLOYED_VERSION == $VERSION ];
         then
+            echo "Running auto update"
+            echo helm upgrade --install -n "$NAMESPACE" "$TARGETNAME" .
             helm upgrade --install -n "$NAMESPACE" "$TARGETNAME" .
             if [ $? -ne 0 ];
             then
                 echo "Failed to upgrade helm chart"
                 exit 1
+            else 
+                echo "Update completed"
+                helm list -n "$NAMESPACE" -o json -f "$NAME"
             fi
+        else
+            echo "Deployed version $DEPLOYMENT_VERSION does not match $VERSION, skipping auto update"
+            return;
         fi
+    else
+        echo "Not building the master branch, not auto updating"
+        return;
     fi
 }
